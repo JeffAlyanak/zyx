bdf31a803fc28242362cfba717788d47fdf152a6
Jeff Alyanak <jeff@alyanak.ca>
2018-10-09 18:06:10 -0400

    Cleaning up of poor formatting.

--- zeropage.s
+++ zeropage.s
@@ -8,9 +8,9 @@
 
 .segment "ZEROPAGE"
 
-nmi_counter:	.res 1	; Counts DOWN for each vblank.
-txt_ptr:        .res 1	; Points to the next character to fetch from a message.
-chr_ptr_x:      .res 1  ; Points to a tile to grab.
-chr_ptr_y:      .res 1  ;
-screen_offset:	.res 1	; Points to the next screen offset to write.
-misc_counter:   .res 1
+nmi_counter:			.res 1	; Counts DOWN for each vblank.
+txt_ptr:				.res 1	; Points to the next character to fetch from a message.
+chr_ptr_x:				.res 1	; Points to a tile to grab.
+chr_ptr_y:				.res 1
+screen_offset:			.res 1	; Points to the next screen offset to write.
+misc_counter:			.res 1
